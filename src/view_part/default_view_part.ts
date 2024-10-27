725bf19b0503e9eae825153aa179441df16a8b3f
sungmin <buttonfly@gmail.com>
2024-10-28 03:57:25 +0900

    hotfix

--- src/view_part/default_view_part.ts
+++ src/view_part/default_view_part.ts
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from 'three/tsl'
 import { ViewPart } from "./view_part";
 import { Light } from '../widget/light';
 import { Control } from '../control';
--- src/widget/light.ts
+++ src/widget/light.ts
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from 'three/tsl'
 
 const enable_light_helper: boolean = false
 
